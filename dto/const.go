f3e61d17b963a17ce34478e7db7d1b4e4fa126a4
zenpk <99558412+zenpk@users.noreply.github.com>
2024-04-28 12:50:20 +0000

    finish chat handler

---
+++ dto/const.go
@@ -0,0 +1,5 @@
+package dto
+
+const (
+	MessageEnding = "[DONE]"
+)
--- handler/openai.go
+++ handler/openai.go
@@ -6,13 +6,33 @@ import (
 )
 
 func (h *Handler) chat(c echo.Context) error {
+	const ChanSize = 1024
 	req := new(dto.OpenAiReqFromClient)
 	if err := c.Bind(req); err != nil {
 		return err
 	}
 	uuid := c.Get("uuid").(string)
-	// TODO
-	replyChan := make(chan string)
-	h.openAiService.Chat(uuid, req, replyChan)
-	return nil
+	replyChan := make(chan string, ChanSize)
+	errChan := make(chan error, 1)
+	c.Response().Header().Set(echo.HeaderContentType, "text/event-stream")
+	c.Response().Header().Set(echo.HeaderCacheControl, "no-cache")
+	c.Response().Header().Set(echo.HeaderConnection, "keep-alive")
+	go func() {
+		if err := h.openAiService.Chat(uuid, req, replyChan); err != nil {
+			errChan <- err
+		}
+	}()
+	for {
+		select {
+		case reply := <-replyChan:
+			if _, err := c.Response().Write([]byte(reply)); err != nil {
+				return err
+			}
+			if reply == dto.MessageEnding {
+				return nil
+			}
+		case err := <-errChan:
+			return err
+		}
+	}
 }
